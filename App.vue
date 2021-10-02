d52d2dfa679feead7e114fcce9bc95861ffebfd1
K0NN3CT10N <konnection.official@gmail.com>
2021-10-02 16:48:46 -0400

    Added files for week 3 of Software Saturdays
    Learned concepts in week 3: Creating Vue projects with npm, implementing components and Vue instances in npm projects, running everything via terminal

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ToDoList></ToDoList>
+    <Demo2></Demo2>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
+import Demo2 from './components/Demo2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Demo2,
+    ToDoList,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ToDoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>{{todo.text}}</li>
+</template>
+
+<script>
+export default {
+    name: 'ToDoItem',
+    props: {
+        todo: Object
+    }
+}
+</script>
+
+<style>
+</style>
